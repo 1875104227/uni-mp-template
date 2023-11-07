c09ecb81bd9486edd40cbe307beae91cf56aa1ae
“yaaii” <18751074229@163.com>
2023-11-07 11:46:11 +0800

    模拟快捷登录

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,9 +5,9 @@
 <template>
   <view class="index">index</view>
   <div class="flex justify-center h-100 items-center">
-    <div class="w-10 h-10 bg-pink-500"></div>
+    <div class="w-30 h-30 bg-pink-500"></div>
   </div>
-  <div class="test bg-yellow-300 w-700 h-10"></div>
+  <div class="test bg-yellow-300 w-730 h-100"></div>
 </template>
 <style lang="scss">
 //
