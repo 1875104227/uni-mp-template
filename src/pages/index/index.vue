f4f31cc44c3ebdf635981c1b832cda28b04288c2
“yaaii” <18751074229@163.com>
2023-11-07 10:09:04 +0800

    add dep

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="index">index</view>
+  <div class="flex justify-center h-100 items-center">
+    <div class="w-10 h-10 bg-pink-500"></div>
+  </div>
+  <div class="test bg-yellow-300 w-700 h-10"></div>
+</template>
+<style lang="scss">
+//
+</style>
