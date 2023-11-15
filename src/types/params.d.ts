378a76f37d70fbb5973f6bf0755f40036d6d0ff9
“yaaii” <18751074229@163.com>
2023-11-15 21:40:09 +0800

    用户模块更正

--- src/types/params.d.ts
+++ src/types/params.d.ts
@@ -35,9 +35,15 @@ declare type ProfileDetail = {
   fullLocation?: string
   /** 职业 */
   profession?: string
+  /** 省份编码 */
+  provinceCode?: string
+  /** 城市编码 */
+  cityCode?: string
+  /** 区/县编码 */
+  countyCode?: string
 }
 /** 性别 */
-declare type Gender = '女' | '男'
+declare type Gender = 1 | 0
 
 /** 个人信息修改请求体参数 */
 declare type ProfileParams = Pick<ProfileDetail, 'nickname' | 'gender' | 'birthday' | 'profession'> & {
