a4861efa99c7d088cf33c98287e13e3e2680939d
“yaaii” <18751074229@163.com>
2023-11-18 15:23:20 +0800

    地址模块

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { onLoad } from '@dcloudio/uni-app'
 import AddressPanel from './components/AddressPanel.vue'
-import ServicePanel from './components/ServicePanel.vue'
-
+import ServicePanelVue from './components/ServicePanel.vue'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
-// 接收页面参数
 const query = defineProps<{
   id: string
 }>()
-// 获取商品详情信息
+
+// 获取商品详细信息
 const goods = ref<GoodsResult>()
 const getGoodsByIdData = async () => {
   const res = await getGoodsByIdAPI(query.id)
@@ -21,31 +19,32 @@ onLoad(() => {
   getGoodsByIdData()
 })
 
-// 轮播变化时
+//轮播变化时
 const currentIndex = ref(0)
 const onChange: UniHelper.SwiperOnChange = (ev) => {
   currentIndex.value = ev.detail.current
 }
-// 点击图片时
+
 const onTapImage = (url: string) => {
-  // 大图预览
+  //大图预览
   uni.previewImage({
     current: url,
     urls: goods.value!.mainPictures
   })
 }
-// uni-ui 弹出层组件 ref
+
+//uni-ui 弹出层组件 ref
 const popup = ref<{
   open: (type?: UniHelper.UniPopupType) => void
   close: () => void
 }>()
 
-// 弹出层条件渲染
+//弹出层条件渲染
 const popupName = ref<'address' | 'service'>()
 const openPopup = (name: typeof popupName.value) => {
-  // 修改弹出层名称
+  //修改弹出层名称
   popupName.value = name
-  // 打开弹出层
+  //打开弹出层
   popup.value?.open()
 }
 </script>
@@ -84,21 +83,22 @@ const openPopup = (name: typeof popupName.value) => {
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view @tap="openPopup('address')" class="item arrow">
+        <view class="item arrow" @tap="openPopup('address')">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view @tap="openPopup('service')" class="item arrow">
+        <view class="item arrow" @tap="openPopup('service')">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
       </view>
     </view>
-    <!-- uni-ui 弹出层 -->
+
     <uni-popup ref="popup" type="bottom" background-color="#fff">
       <AddressPanel v-if="popupName === 'address'" @close="popup?.close()" />
-      <ServicePanel v-if="popupName === 'service'" @close="popup?.close()" />
+      <ServicePanelVue v-if="popupName === 'service'" @close="popup?.close()" />
     </uni-popup>
+
     <!-- 商品详情 -->
     <view class="detail panel">
       <view class="title">
